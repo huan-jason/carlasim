5bdacbf911a6c7e1e4d22eb013e6586770484ba1
Jason Huan <huan.jason@gmail.com>
2024-09-19 18:47:28 +0800

    2024-09-19-1847

--- carlasim.sh
+++ carlasim.sh
@@ -3,6 +3,7 @@ set -eu
 
 docker run -it --rm \
    -e DISPLAY=$DISPLAY \
+   -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
    -v $HOME/.Xauthority:/root/.Xauthority \
    -v /tmp/.X11-unix:/tmp/.X11-unix \
    huanjason/carlasim
